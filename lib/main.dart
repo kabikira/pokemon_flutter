04f9ee13ba28708da3e90fca0fa84a28ce6ce45a
kabikira <kabikirar2@protonmail.com>
2025-03-08 19:01:01 +0900

    Step2

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,45 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('HelloWorld!', style: TextStyle(fontSize: 24)),
+      body: Container(
+        padding: const EdgeInsets.all(32),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(32),
+                    child: Image.network(
+                      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+                      height: 100,
+                      width: 100,
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: const Text(
+                      'No.25',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Text(
+                'pikachu',
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              ),
+              const Chip(
+                label: Text('electric'),
+                backgroundColor: Colors.yellow,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
