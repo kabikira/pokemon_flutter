984ab622572db755bbc586885767741d13d1bead
kabikira <kabikirar2@protonmail.com>
2025-03-16 11:21:59 +0900

    Step9 ボトムシート表示、お気に入り切り替え

--- lib/poke_list.dart
+++ lib/poke_list.dart
@@ -3,6 +3,7 @@ import 'package:pokemon_flutter/consts/pokeapi.dart';
 import 'package:pokemon_flutter/models/favorite.dart';
 import 'package:pokemon_flutter/models/pokemon.dart';
 import 'package:pokemon_flutter/poke_list_item.dart';
+import 'package:pokemon_flutter/view_mode_bttom_sheet.dart';
 import 'package:provider/provider.dart';
 
 class PokeList extends StatefulWidget {
@@ -63,11 +64,26 @@ class _PokeListState extends State<PokeList> {
           alignment: Alignment.topRight,
           child: IconButton(
             padding: const EdgeInsets.all(0),
-            icon:
-                isFavoriteMode
-                    ? const Icon(Icons.star, color: Colors.orangeAccent)
-                    : const Icon(Icons.star_outline),
-            onPressed: () => {setState(() => isFavoriteMode = !isFavoriteMode)},
+            icon: const Icon(Icons.auto_awesome_outlined),
+            onPressed: () async {
+              var ret = await showModalBottomSheet<bool>(
+                context: context,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+                builder: (BuildContext context) {
+                  return ViewModeBottomSheet(favMode: isFavoriteMode);
+                },
+              );
+              if (ret != null && ret) {
+                setState(() {
+                  isFavoriteMode = !isFavoriteMode;
+                });
+              }
+            },
           ),
         ),
         Expanded(
